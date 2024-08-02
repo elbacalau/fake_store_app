8791af3a92ae43dabb42f96dab512be76ed24dd2
Elbacalau <oriolreverte36@gmail.com>
2024-08-02 17:36:59 +0200

    Added DDLimitSelector

--- lib/presentation/screens/shop_page/shop_page.dart
+++ lib/presentation/screens/shop_page/shop_page.dart
@@ -1,5 +1,4 @@
 import 'package:fake_store_app/presentation/providers/product_list_provider.dart';
-import 'package:fake_store_app/presentation/screens/shop_page/filter_product_widget/drop_filter_menu.dart';
 import 'package:fake_store_app/presentation/screens/shop_page/products_card/products_card_2_col.dart';
 import 'package:fake_store_app/presentation/screens/shop_page/products_card/products_card_3_col.dart';
 import 'package:flutter/material.dart';
@@ -14,12 +13,15 @@ class ShopPage extends StatefulWidget {
 }
 
 class _ShopPageState extends State<ShopPage> {
-  // fuera del arbol de widget para poder actualizar el estado
   int selectedCrossASize = 2;
+  int defaultLimit = 5;
+
   @override
   void initState() {
     super.initState();
-    Provider.of<ProductListProvider>(context, listen: false).fetchProducts();
+    final listProducts =
+        Provider.of<ProductListProvider>(context, listen: false);
+    listProducts.fetchProducts(defaultLimit);
   }
 
   @override
@@ -46,7 +48,63 @@ class _ShopPageState extends State<ShopPage> {
               children: [
                 Row(
                   children: [
-                    const DropFilterMenu(),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 2.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(color: Colors.grey),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton<int>(
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          value: defaultLimit,
+                          onChanged: (int? value) {
+                            setState(() {
+                              defaultLimit = value!;
+                            });
+                            Provider.of<ProductListProvider>(context,
+                                    listen: false)
+                                .fetchProducts(defaultLimit);
+                          },
+                          items: const [
+                            DropdownMenuItem<int>(
+                              value: 5,
+                              child: Row(
+                                children: [
+                                  Icon(Iconsax.box, color: Colors.black),
+                                  SizedBox(width: 8.0),
+                                  Text('5 Productos'),
+                                ],
+                              ),
+                            ),
+                            DropdownMenuItem<int>(
+                              value: 10,
+                              child: Row(
+                                children: [
+                                  Icon(Iconsax.box, color: Colors.black),
+                                  SizedBox(width: 8.0),
+                                  Text('10 Productos'),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 8.0),
                       child: Container(
@@ -119,12 +177,7 @@ class _ShopPageState extends State<ShopPage> {
           Expanded(
               child: selectedCrossASize == 2
                   ? const ProductsCard2Col(crossSize: 2)
-                  : const ProductCard3Col(crossSize: 3)
-
-              // child: ProductsCard(
-              //   crossSize: selectedCrossASize,
-              // ),
-              ),
+                  : const ProductCard3Col(crossSize: 3)),
         ],
       ),
     );
