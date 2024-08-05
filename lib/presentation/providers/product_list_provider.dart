f47774f8b01b8aac0ce50943b3f780117d780099
Elbacalau <oriolreverte36@gmail.com>
2024-08-05 17:27:42 +0200

    Count all prod to calc default Limit

--- lib/presentation/providers/product_list_provider.dart
+++ lib/presentation/providers/product_list_provider.dart
@@ -5,12 +5,15 @@ import 'package:flutter/material.dart';
 class ProductListProvider with ChangeNotifier {
   List<Product> _products = [];
   final _getListProducts = GetListProducts();
+  int _totalProductsCount = 0;
 
   List<Product> get products => _products;
+  int get totalProductsCount => _totalProductsCount;
 
-  Future<void> fetchProducts(int limit) async {
+  Future<void> fetchProducts([int? limit]) async {
     try {
       _products = await _getListProducts.getListProducts(limit);
+      _totalProductsCount = _products.length;
       notifyListeners();
     } catch (e) {
       rethrow;
