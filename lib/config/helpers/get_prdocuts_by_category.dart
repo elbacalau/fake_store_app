a2c444cee2e270aea075e1aff448a02bebed03c9
Elbacalau <oriolreverte36@gmail.com>
2024-08-09 15:33:12 +0200

    Refactor GetProductsByCategory to add input validation, error handling, and timeout settings for Dio requests.

--- lib/config/helpers/get_prdocuts_by_category.dart
+++ lib/config/helpers/get_prdocuts_by_category.dart
@@ -3,20 +3,49 @@ import 'package:fake_store_app/domain/entities/product.dart';
 import 'package:fake_store_app/infrastructure/models/product_model.dart';
 
 class GetProductsByCategory {
-  final _dio = Dio();
+  final Dio _dio;
   final String baseUrl = 'https://fakestoreapi.com/products/category/';
 
+  GetProductsByCategory([Dio? dio])
+      : _dio = dio ??
+            Dio(
+              BaseOptions(
+                connectTimeout: const Duration(seconds: 10),
+                receiveTimeout: const Duration(seconds: 10),
+                responseType: ResponseType.json,
+                followRedirects: false,
+              ),
+            );
+
   Future<List<Product>> getProdByCategory([String? category]) async {
-    final response = await _dio.get('$baseUrl$category');
+    if (category == null || category.isEmpty) {
+      throw ArgumentError('Category cannot be null or empty');
+    }
 
-    List<dynamic> data = response.data;
+    try {
+      final response = await _dio.get('$baseUrl$category');
 
-    List<ProductModel> productModel = ProductModel.fromJsonList(data);
+      if (response.statusCode == 200) {
+        List<dynamic> data = response.data;
 
-    List<Product> products = productModel.map((p) {
-      return p.toProduct();
-    }).toList();
+        List<ProductModel> productModel = ProductModel.fromJsonList(data);
 
-    return products;
+        return productModel.map((p) => p.toProduct()).toList();
+      } else {
+        throw Exception('Failed to load products');
+      }
+    } on DioException catch (e) {
+      if (e.type == DioExceptionType.connectionTimeout) {
+        throw Exception('Connection Timeout');
+      } else if (e.type == DioExceptionType.receiveTimeout) {
+        throw Exception('Receive Timeout');
+      } else if (e.type == DioExceptionType.badResponse) {
+        throw Exception('Invalid status code: ${e.response?.statusCode}');
+      } else {
+        throw Exception('Failed to load products: ${e.message}');
+      }
+    } catch (e) {
+      throw Exception('Unexpected error: $e');
+    }
   }
 }
